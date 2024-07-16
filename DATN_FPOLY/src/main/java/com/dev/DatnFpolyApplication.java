7ffad00f0347e9c040995d4ee322f81477e207fe
PhamNhatHoang <1st.phamnhathoang@gmail.com>
2024-07-16 14:10:46 +0700

    Test Commit

---
+++ DATN_FPOLY/src/main/java/com/dev/DatnFpolyApplication.java
@@ -0,0 +1,13 @@
+package com.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnFpolyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnFpolyApplication.class, args);
+	}
+
+}
---
+++ DATN_FPOLY/src/test/java/com/dev/DatnFpolyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnFpolyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
