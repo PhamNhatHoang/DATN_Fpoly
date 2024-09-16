a50e3099862840c251fe96f5936b467e5727b32b
TrongPhuc <nguyentrongphuc03@gmail.com>
2024-09-16 14:14:27 +0700

    update source

--- PetShop/src/main/java/com/PetShopApplication.java
+++ PetShop/src/main/java/com/PetShopApplication.java
@@ -1,13 +1,13 @@
-package com.dev;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class PetShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(PetShopApplication.class, args);
 	}
 
 }
---
+++ PetShop/src/main/java/com/entity/User.java
@@ -0,0 +1,39 @@
+package com.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @Column(name = "UserName")
+    private String username;
+
+    @Column(name = "UserPassword")
+    private String userPassword;
+
+    @Column(name = "FullName")
+    private String fullName;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "PhoneNumber")
+    private String phoneNumber;
+
+    @Column(name = "UserAddress")
+    private String userAddress;
+
+    @ManyToOne
+    @JoinColumn(name = "AuthID")
+    private Authority authority;
+
+}
