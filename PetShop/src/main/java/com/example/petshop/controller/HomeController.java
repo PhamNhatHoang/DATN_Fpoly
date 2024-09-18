ee8134d69d561fdf5e4fcd80b4301084312ba146
Chemyth <ngovietanh121@gmail.com>
2024-09-18 14:44:00 +0700

    demo getAll users

--- PetShop/src/main/java/com/example/petshop/controller/HomeController.java
+++ PetShop/src/main/java/com/example/petshop/controller/HomeController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -14,6 +17,8 @@ public class HomeController {
 
     @RequestMapping({"/", "/trang-chu", "/home"})
     public String home(Model model) {
+        List<User> userList = userService.findAll();
+        model.addAttribute("userList", userList);
         return "home";
     }
 }
