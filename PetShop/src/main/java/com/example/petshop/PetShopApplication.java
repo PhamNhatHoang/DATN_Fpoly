5923535c79746e5a5daa30ba70900a8d65cfdda1
Chemyth <ngovietanh121@gmail.com>
2024-09-18 19:19:56 +0700

    update source

---
+++ PetShop/src/main/java/com/example/petshop/PetShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetShopApplication.class, args);
+    }
+
+}
