63345adcba95399c167b19704db1fc77d84789cf
TrongPhuc <nguyentrongphuc03@gmail.com>
2024-10-09 15:46:14 +0700

    fixpetde

---
+++ PetShop/src/main/java/com/example/petshop/controller/PetController.java
@@ -0,0 +1,43 @@
+package com.example.petshop.controller;
+
+import com.example.petshop.entity.Pet;
+import com.example.petshop.service.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin("*")
+@RequestMapping("/api/pet")
+@RestController
+public class PetController {
+    @Autowired
+    private PetService petService;
+
+    @GetMapping
+    public List<Pet> getPet() {
+       return petService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Pet> getPetId(@PathVariable("id") String id){
+       return petService.findById(id);
+    }
+
+    @PostMapping
+    public void save(@RequestBody Pet pet) {
+        petService.save(pet);
+    }
+
+    @PutMapping("/{id}")
+    public void updatePet(@PathVariable("id") String id, @RequestBody Pet pet) {
+        pet.setPetID(id);
+        petService.save(pet);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePet(@PathVariable("id") String id) {
+        petService.deleteById(id);
+    }
+}
--- PetShop/src/main/java/com/example/petshop/service/PetService.java
+++ PetShop/src/main/java/com/example/petshop/service/PetService.java
@@ -1,7 +1,15 @@
 package com.example.petshop.service;
 
+import com.example.petshop.entity.Pet;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface PetService {
+    List<Pet> getAll();
+    Optional<Pet> findById(String id);
+    void save(Pet pet);
+    void deleteById(String id);
 }
