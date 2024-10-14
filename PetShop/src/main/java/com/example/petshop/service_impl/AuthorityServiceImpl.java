bfdde0e7bf824bc05b7c76307cf9e5fe280e439e
tamXinChao <dominhtam0175@gmail.com>
2024-10-14 22:53:43 +0700

    fix merge

--- PetShop/src/main/java/com/example/petshop/service_impl/AuthorityServiceImpl.java
+++ PetShop/src/main/java/com/example/petshop/service_impl/AuthorityServiceImpl.java
@@ -1,15 +1,11 @@
 package com.example.petshop.service_impl;
 
-<<<<<<< HEAD
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.example.petshop.entity.Authority;
 import com.example.petshop.entity.DTO.AuthorityDTO;
-=======
-import com.example.petshop.entity.Authority;
->>>>>>> f01df3a5e39dfa4d0214dcb95bf600e5759b0a39
 import com.example.petshop.repo.AuthorityRepo;
 import com.example.petshop.service.AuthorityService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +15,7 @@ import java.util.List;
 
 @Service
 public class AuthorityServiceImpl implements AuthorityService {
-<<<<<<< HEAD
-    
-=======
+
     @Autowired
     private AuthorityRepo authorityRepo;
 
@@ -29,5 +23,4 @@ public class AuthorityServiceImpl implements AuthorityService {
     public List<Authority> findAll() {
         return authorityRepo.findAll();
     }
->>>>>>> f01df3a5e39dfa4d0214dcb95bf600e5759b0a39
 }
