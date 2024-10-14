a3f39974d83057e7b72cb02714d1904f53d71085
Anh Anh <ngovietanh121@gmail.com>
2024-10-15 02:35:43 +0700

    add button image, fix layout,new look

--- PetShop/src/main/java/com/example/petshop/controller/HomeController.java
+++ PetShop/src/main/java/com/example/petshop/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.example.petshop.controller;
 
+import com.example.petshop.entity.Pet;
 import com.example.petshop.entity.Product;
 import com.example.petshop.entity.User;
+import com.example.petshop.service.PetService;
 import com.example.petshop.service.ProductService;
 import com.example.petshop.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -25,6 +28,9 @@ public class HomeController {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private PetService petService;
+
     @ModelAttribute("fullname")
     public void getUser(Model model, HttpServletRequest request) {
         try {
@@ -40,15 +46,15 @@ public class HomeController {
     @RequestMapping({"/", "/trang-chu", "/home"})
     public String home(Model model) {
         List<Product> productsList = productService.getAll();
-        Product latestProduct = productsList.stream()
-                .max(Comparator.comparingInt(Product::getId))
-                .orElseThrow(() -> new NoSuchElementException("No product found"));
-        Collection<Product> nextSixProducts = productsList.stream()
-                .skip(1)
-                .limit(6)
-                .collect(Collectors.toList());
+        Product latestProduct = productsList.stream().max(Comparator.comparingInt(Product::getId)).orElseThrow(() -> new NoSuchElementException("No product found"));
+        Collection<Product> nextSixProducts = productsList.stream().skip(1).limit(6).collect(Collectors.toList());
+        List<Pet> petList = petService.getAll();
+        Pet latestPet = petList.stream().max(Comparator.comparing(Pet::getPetID)).orElseThrow(() -> new NoSuchElementException("No pet found"));
+        Collection<Pet> nextSixPet = petList.stream().skip(1).limit(6).toList();
         model.addAttribute("firstProduct", latestProduct);
         model.addAttribute("nextSixProducts", nextSixProducts);
+        model.addAttribute("firstPet", latestPet);
+        model.addAttribute("nextSixPet", nextSixPet);
         return "/layout/_main";
     }
 
@@ -68,9 +74,7 @@ public class HomeController {
     }
 
     @RequestMapping("/login")
-    public String login(Model model,
-                        @RequestParam(value = "error", required = false) boolean error,
-                        @RequestParam(value = "success", required = false) boolean success) {
+    public String login(Model model, @RequestParam(value = "error", required = false) boolean error, @RequestParam(value = "success", required = false) boolean success) {
         if (error) {
             model.addAttribute("message", "Đăng nhập thất bại!");
             model.addAttribute("loginStatus", false);
